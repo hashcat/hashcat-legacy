bd2a09ef794108af81ed41c608234cdd56817650
Gabriele 'matrix' Gristina <gm4tr1x@users.noreply.github.com>
2016-01-11 01:09:59 +0100

    Added autotools support, fixed issues #46 #34 and #15

--- tools/deps.sh
+++ tools/deps.sh
@@ -14,20 +14,18 @@ deps_dir=${script_directory}/../deps
 mkdir -p ${deps_dir} # but it should already exist (is part of the repository)
 cd ${deps_dir}
 
-## root check
-if [ $(id -u) -ne 0 ]; then
-  echo "! Must be root"
-  exit 1
-fi
+if [ $# -gt 0 ]; then
+  if [ "$1" == "clean" ]; then
+    ## make a fresh "deps" workspace
+    rm -rf tmp/gmp* gmp/{linux32,linux64,win32,win64,osx64} && \
+    mkdir -p gmp/{linux32,linux64,win32,win64,osx64} tmp && \
+    cd tmp
 
-## make a fresh "deps" directories
-rm -rf tmp/gmp* gmp/{linux32,linux64,win32,win64,osx64} && \
-mkdir -p gmp/{linux32,linux64,win32,win64,osx64} tmp && \
-cd tmp
-
-if [ $? -ne 0 ]; then
-  echo "! Cannot create the deps directories."
-  exit 1
+    if [ $? -ne 0 ]; then
+      echo "! Cannot cleanup the deps workspace."
+      exit 1
+    fi
+  fi
 fi
 
 ## install osx cross stuff
@@ -44,7 +42,7 @@ if [ $? -ne 0 ]; then
     fi
   fi
 
-  dpkg -i libssl0.9.8_0.9.8o-4squeeze14_amd64.deb
+  sudo dpkg -i libssl0.9.8_0.9.8o-4squeeze14_amd64.deb
   if [ $? -ne 0 ]; then
     echo "! failed to install libssl0.9.8"
     exit 1
@@ -62,7 +60,7 @@ if [ $? -ne 0 ]; then
     fi
   fi
 
-  dpkg -i apple-uni-sdk-10.5_20110407-0.flosoft1_amd64.deb
+  sudo dpkg -i apple-uni-sdk-10.5_20110407-0.flosoft1_amd64.deb
   if [ $? -ne 0 ]; then
     echo "! failed to install apple-uni-sdk-10.5"
     exit 1
@@ -79,7 +77,7 @@ if [ $? -ne 0 ]; then
     fi
   fi
 
-  dpkg -i apple-uni-sdk-10.6_20110407-0.flosoft1_amd64.deb
+  sudo dpkg -i apple-uni-sdk-10.6_20110407-0.flosoft1_amd64.deb
   if [ $? -ne 0 ]; then
     echo "! failed to install apple-uni-sdk-10.6"
     exit 1
@@ -96,7 +94,7 @@ if [ $? -ne 0 ]; then
     fi
   fi
 
-  dpkg -i apple-x86-odcctools_758.159-0flosoft11_amd64.deb
+  sudo dpkg -i apple-x86-odcctools_758.159-0flosoft11_amd64.deb
   if [ $? -ne 0 ]; then
     echo "! failed to install apple-x86-odcctools"
     exit 1
@@ -113,7 +111,7 @@ if [ $? -ne 0 ]; then
     fi
   fi
 
-  dpkg -i apple-x86-gcc_4.2.1~5646.1flosoft2_amd64.deb
+  sudo dpkg -i apple-x86-gcc_4.2.1~5646.1flosoft2_amd64.deb
   if [ $? -ne 0 ]; then
     echo "! failed to install apple-x86-gcc"
     exit 1
@@ -123,23 +121,30 @@ fi
 cd ..
 
 ## installing needed packages
-apt-get -y install ${DEPS}
+sudo apt-get -y install ${DEPS}
 if [ $? -ne 0 ]; then
   echo "! failed to install deps packages"
   exit 1
 fi
 
-## download gmp source code
-wget -c https://gmplib.org/download/gmp/${GMP_VER}.tar.lz
-if [ $? -ne 0 ]; then
-  echo "! failed to download GMP source code"
-  exit 1
+cd tmp
+
+if [ ! -f "${GMP_VER}.tar.lz" ]; then
+  ## download gmp source code
+  wget -c https://gmplib.org/download/gmp/${GMP_VER}.tar.lz
+  if [ $? -ne 0 ]; then
+    echo "! failed to download GMP source code"
+    exit 1
+  fi
 fi
 
-tar xf ${GMP_VER}.tar.lz
-if [ $? -ne 0 ]; then
-  echo "! failed to extract GMP source code"
-  exit 1
+if [ ! -d "${GMP_VER}" ]; then
+  ## extract gmp source sode
+  tar xf ${GMP_VER}.tar.lz
+  if [ $? -ne 0 ]; then
+    echo "! failed to extract GMP source code"
+    exit 1
+  fi
 fi
 
 newDir=$(tar tvf ${GMP_VER}.tar.lz | head -n1 | awk '{print $6}' | sed -e 's/\///g')
@@ -147,71 +152,61 @@ if [ "${newDir}" != "${GMP_VER}" ]; then
   mv ${newDir} ${GMP_VER}
 fi
 
-## build gmp lib for linux32
-cp -af ${GMP_VER} ${GMP_VER}-linux32
-cd ${GMP_VER}-linux32 && \
-./configure --host=i386-pc-linux-gnu --prefix=${deps_dir}/gmp/linux32 --disable-shared && \
-sudo make install && \
-cd .. && \
-rm -rf ${GMP_VER}-linux32
+OS_TARGET="linux32 linux64 win32 win64 osx64"
 
-if [ $? -ne 0 ]; then
-  echo "! failed to build linux32 gmp lib."
-  exit 1
-fi
+for os in ${OS_TARGET}; do
 
-## build gmp lib for linux64
-cp -af ${GMP_VER} ${GMP_VER}-linux64
-cd ${GMP_VER}-linux64 && \
-./configure --host=x86_64-pc-linux-gnu --prefix=${deps_dir}/gmp/linux64 --disable-shared && \
-sudo make install && \
-cd .. && \
-rm -rf ${GMP_VER}-linux64
+  ret=1
+  osx=1234
+  # cleanup workspace
+  rm -rf ${GMP_VER}/build && \
+  mkdir -p ${GMP_VER}/build
+  cd ${GMP_VER}/build
 
-if [ $? -ne 0 ]; then
-  echo "! failed to build linux64 gmp lib."
-  exit 1
-fi
+  # handling supported os
+  case $os in
+    linux32)
+      ../configure --host=i386-pc-linux-gnu --prefix=${deps_dir}/gmp/linux32 --disable-shared && make install
+      ;;
+    linux64)
+      ../configure --host=x86_64-pc-linux-gnu --prefix=${deps_dir}/gmp/linux64 --disable-shared && make install
+      ;;
+    win32)
+      ../configure --host=i686-w64-mingw32 --prefix=${deps_dir}/gmp/win32 --disable-shared && make install
+      ;;
+    win64)
+      ../configure --host=x86_64-w64-mingw32 --prefix=${deps_dir}/gmp/win64 --disable-shared && make install
+      ;;
+    osx64)
+      cp ../configure ../configure.legacy
+      sed -i 's/\(i686.*\)$/\1\n\tabilist=64/' ../configure && \
+      ABI=64 ../configure --host=i686-apple-darwin10 --prefix=${deps_dir}/gmp/osx64 --disable-shared --disable-assembly && \
+      make install
+      osx=$?
+      ;;
+    *)
+      echo "! Not supported OS (${os})."
+      exit 1
+      ;;
+  esac
 
-## build gmp lib for win32
-cp -af ${GMP_VER} ${GMP_VER}-win32
-cd ${GMP_VER}-win32 && \
-./configure --host=i686-w64-mingw32 --prefix=${deps_dir}/gmp/win32 --disable-shared && \
-sudo make install && \
-cd .. && \
-rm -rf ${GMP_VER}-win32
+  ret=$?
+  if [ ${osx} != 1234 ]; then
+    mv ../configure.legacy ../configure
+    ret=${osx}
+  fi
 
-if [ $? -ne 0 ]; then
-  echo "! failed to build win32 gmp lib."
-  exit 1
-fi
+  cd ..
 
-## build gmp lib for win64
-cp -af ${GMP_VER} ${GMP_VER}-win64
-cd ${GMP_VER}-win64 && \
-./configure --host=x86_64-w64-mingw32 --prefix=${deps_dir}/gmp/win64 --disable-shared && \
-sudo make install && \
-cd .. && \
-rm -rf ${GMP_VER}-win64
+  if [ ${ret} -ne 0 ]; then
+    echo "! failed to build $os gmp library."
+    exit 1
+  fi
 
-if [ $? -ne 0 ]; then
-  echo "! failed to build win64 gmp lib."
-  exit 1
-fi
+  rm -rf build
+  cd ..
 
-## build gmp lib for osx64
-cp -af ${GMP_VER} ${GMP_VER}-osx64
-cd ${GMP_VER}-osx64 && \
-sed -i 's/\(i686.*\)$/\1\n\tabilist=64/' configure && \
-ABI=64 ./configure --host=i686-apple-darwin10 --prefix=${deps_dir}/gmp/osx64 --disable-shared --disable-assembly && \
-sudo make install && \
-cd .. && \
-rm -rf ${GMP_VER}-osx64
-
-if [ $? -ne 0 ]; then
-  echo "! failed to build osx64 gmp lib."
-  exit 1
-fi
+done
 
 echo
 echo "> Successfully resolved all dependencies for hashcat."
